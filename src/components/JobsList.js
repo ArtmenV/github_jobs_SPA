0a0905298e59edeb09a08d557974922576f3178e
Artmen_V <viliutich.artem@gmail.com>
2019-10-02 17:07:13 +0700

    add_spinner

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -1,13 +1,17 @@
 import React from "react";
-
 import { JobItem } from "./JobItem.js";
-import { useDispatch, useSelector } from "react-redux";
+import { Loader } from "./Loader";
+import { useSelector } from "react-redux";
 import "./JobsList.scss";
 
 export const JobsList = () => {
   const jobsList = useSelector(state => state.JobsReducer.jobs);
+  const loading = useSelector(state => state.JobsReducer);
+  console.log(loading);
 
-  return (
+  return loading.loading === true ? (
+    <Loader />
+  ) : (
     <div className="container justify-content-center col-md-10">
       <ul className="jobs__list">
         {jobsList.map(job => {
---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Loader = () => {
+  return (
+    <div class="text-center">
+      <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
+};
